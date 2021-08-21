7f69e7d88bf545364d7ac71c6a34dcd8dc007268
camilabertinat <bertinatcamila@gmail.com>
2021-08-21 17:36:33 -0300

    Entrega 01

--- js/login.js
+++ js/login.js
@@ -2,5 +2,9 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+    document.getElementById("formu").addEventListener("submit", function(e){
+        e.preventDefault();
+        window.location.assign("inicio.html");
+    });
 });
+function onSignIn(googleUser){var profile = googleUser.getBasicProfile()}
